80e3d3c95b56f3b7d017d6e2b79f521f66e2a3fe
ashmna <mnatsakanyan.ashot@gmail.com>
2014-05-16 23:44:12 +0400

    Player

---
+++ src/Player.js
@@ -0,0 +1,15 @@
+function Player()
+{
+    var level = 1;
+    var points = 0;
+
+    this.getLevel = function()
+    {
+        return level;
+    };
+
+    this.addPoint = function(point)
+    {
+        points += point;
+    };
+}
