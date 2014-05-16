3923ba6b2f47854ae69c396e45bd6be4fd0b1ef7
ashmna <mnatsakanyan.ashot@gmail.com>
2014-05-16 23:05:10 +0400

    Layout

---
+++ src/Layout.js
@@ -0,0 +1,14 @@
+function Layout()
+{
+    var layout;
+    var horizontalCount = 0;
+    var verticalCount = 0;
+
+
+    this.newLayout = function(horizontal, vertical)
+    {
+        horizontalCount = horizontal;
+        verticalCount = vertical;
+        //layout =
+    };
+}
