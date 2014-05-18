cd5da268881583d851a3905fc24f85c4a9cb89af
ashmna <mnatsakanyan.ashot@gmail.com>
2014-05-18 21:04:42 +0400

    done

--- src/Item.js
+++ src/Item.js
@@ -4,6 +4,12 @@ function Item(param)
     var game;
     var item;
     var show = false;
+    var interval;
+    var timeOut;
+    var startTime;
+    var showCount = 0;
+    var point = 0;
+
     var createItem = function()
     {
         var itemCode = '<div class="item">'+
@@ -29,24 +35,56 @@ function Item(param)
     this.show = function()
     {
         item.addClass('flipped');
+        timeOut = setTimeout(this.hide, interval);
         show = true;
+        startTime = new Date().getTime();
+        showCount++;
     };
 
     this.hide = function()
     {
         show = false;
         item.removeClass('flipped');
+        clearTimeout(timeOut);
     };
+
+    this.done = function()
+    {
+        clearTimeout(timeOut);
+        item.find('.front').css({'background': 'black'});
+        var endTime = new Date().getTime();
+        var timePoint = interval - (endTime - startTime);
+        point = timePoint / showCount;
+        //console.log('timePoint [ '+timePoint+' ]  point [  '+point+'  ]');
+    };
+
+    this.getPoint = function()
+    {
+        return point;
+    };
+
     this.setGame = function(gameInstance)
     {
         game = gameInstance;
     };
+    this.setInterval = function(setInterval)
+    {
+        interval = setInterval;
+    };
+
+
     this.getType = function()
     {
         return param.type;
     };
+
     this.isShow = function()
     {
         return show;
     };
+
+    this.element = function()
+    {
+        return item;
+    }
 }
